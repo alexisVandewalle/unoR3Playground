38182a7ecc00fb26be6b51767f98b8de0f5a5d96
Alexis Vandewalle <alexivande60@gmail.com>
2026-02-02 17:09:14 +0100

    blinkLed - compilation and basic program

---
+++ blinkLed/blinkLed.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#define F_CPU 16000000 // set CPU clock to 16MHz (used by _delay_ms)
+#include <util/delay.h>
+
+int main(void){
+    // set PIN B5 as output
+    DDRB |= _BV(DDB5);
+    while(1){
+        // set turn led on/off setting value of port B5
+        PORTB |= _BV(PORTB5);
+        _delay_ms(500);
+        PORTB &= ~_BV(PORTB5);
+        _delay_ms(500);
+
+    }
+    return 0;
+}
