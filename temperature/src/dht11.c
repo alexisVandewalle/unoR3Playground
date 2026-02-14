799e879f4ea6143d069bb19a127140c6952f4287
Alexis Vandewalle <50923846+alexisVandewalle@users.noreply.github.com>
2026-02-14 11:47:03 +0100

    Improve dht11 code

--- temperature/src/dht11.c
+++ temperature/src/dht11.c
@@ -3,15 +3,15 @@
 #include <util/delay.h>
 #include "dht11.h"
 
-void dht11_sendStartSignal(volatile uint8_t* dirReg, uint8_t dirRegBit, volatile uint8_t* port, char portBit){
+void dht11_sendStartSignal(dht11IOSetup setup){
     // set port D7 as output
-    *dirReg |= _BV(dirRegBit);
+    *(setup.dirReg) |= _BV(setup.pin);
     // pull down data wire
-    *port &= ~_BV(portBit);
+    *(setup.portReg) &= ~_BV(setup.pin);
     // wait enough time before pulling data bus up
     _delay_ms(DHT11_START_SIG_DUR_MS);
     // set port as input
-    *dirReg &= ~_BV(dirRegBit);
+    *(setup.dirReg) &= ~_BV(setup.pin);
 }
 
 void dht11_configTimer(){
@@ -21,23 +21,23 @@ void dht11_configTimer(){
     TCCR0B = 3;
 }
 
-uint8_t dht11_getData(volatile uint8_t* dirReg, uint8_t dirRegBit, volatile uint8_t* pin, volatile uint8_t pinBit, uint8_t* temp, uint8_t* humidity){
+uint8_t dht11_getData(dht11IOSetup setup, uint8_t* tempI, uint8_t* tempD, uint8_t* humidity){
     // wait response signal
     TCNT0 = 0;
-    while((*pin & _BV(pinBit))){
+    while((*(setup.pinReg) & _BV(setup.pin))){
         if(TCNT0>DHT11_TIMEOUT){
             return DHT11_TIMEOUT_CODE;
         }
     };
     // wait end of response signal
     TCNT0 = 0;
-    while(!(*pin & _BV(pinBit))){
+    while(!(*(setup.pinReg) & _BV(setup.pin))){
         if(TCNT0>DHT11_TIMEOUT){
             return DHT11_TIMEOUT_CODE;
         }
     }
     TCNT0 = 0;
-    while((*pin & _BV(pinBit))){
+    while((*(setup.pinReg) & _BV(setup.pin))){
         if(TCNT0>DHT11_TIMEOUT){
             return DHT11_TIMEOUT_CODE;
         }
@@ -48,14 +48,14 @@ uint8_t dht11_getData(volatile uint8_t* dirReg, uint8_t dirRegBit, volatile uint
     for(int8_t i=0; i<DHT11_N_BITS; i++){
         // wait start of bit transmission
         TCNT0 = 0;
-        while(!(*pin & _BV(pinBit))){
+        while(!(*(setup.pinReg) & _BV(setup.pin))){
             if(TCNT0>DHT11_TIMEOUT){
                 return DHT11_TIMEOUT_CODE;
             }
         }
         // reset timer 0
         TCNT0 = 0;
-        while((*pin & _BV(pinBit))){
+        while((*(setup.pinReg) & _BV(setup.pin))){
             if(TCNT0>DHT11_TIMEOUT){
                 return DHT11_TIMEOUT_CODE;
             }
@@ -66,19 +66,16 @@ uint8_t dht11_getData(volatile uint8_t* dirReg, uint8_t dirRegBit, volatile uint
             idxData++;
         }
     }
-    // keep only integral part, decimal part not relevant
+    // keep only integral part for humidity, decimal part not relevant
     // with respect to sensor accuracy
     *humidity = data[0];
-    *temp = data[2];
+    *tempI = data[2];
+    *tempD = data[3];
     return (data[0]+data[1]+data[2]+data[3])==data[4];
 }
 
-uint8_t dht11_measure(
-            volatile uint8_t* dirReg, uint8_t dirRegBit, 
-            volatile uint8_t* port, char portBit,
-            volatile uint8_t* pin, volatile uint8_t pinBit, 
-            uint8_t* temp, uint8_t* humidity){
+uint8_t dht11_measure(dht11IOSetup setup, uint8_t* tempI, uint8_t* tempD, uint8_t* humidity){
     dht11_configTimer();
-    dht11_sendStartSignal(dirReg, dirRegBit, port, portBit);
-    return dht11_getData(dirReg, dirRegBit, pin, pinBit, temp, humidity);
+    dht11_sendStartSignal(setup);
+    return dht11_getData(setup, tempI, tempD, humidity);
 }
--- temperature/src/temperature.c
+++ temperature/src/temperature.c
@@ -12,13 +12,14 @@ void initio(){
 
 int main(void){
     initUART(9600);
-    uint8_t temp, humidity;
+    dht11IOSetup setup = {&DDRD, &PORTD, &PIND, PIND2};
+    uint8_t tempI, tempD, humidity;
     char buffer[100] = {'\0'};
     while(1){
-        uint8_t status = dht11_measure(&DDRD, DDD7,&PORTD, PORTD7, &PIND, PIND7, &temp, &humidity);
-        sprintf(buffer, "T=%d H=%d status=%d\n", temp, humidity, status);
+        uint8_t status = dht11_measure(setup, &tempI, &tempD, &humidity);
+        sprintf(buffer, "T=%d.%d H=%d status=%d\n", tempI, tempD, humidity, status);
         transmitUARTStr(buffer);
-        _delay_ms(10000);
+        _delay_ms(5000);
     }
     return 0;
 }
